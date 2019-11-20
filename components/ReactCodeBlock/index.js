30f0ec3246ee24b97227966564e27a22ca9c47da
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-11-20 18:02:19 +0300

    removed unused modules from syntax highlighter component

--- components/ReactCodeBlock/index.js
+++ components/ReactCodeBlock/index.js
@@ -1,9 +1,6 @@
 
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import okaidia from 'react-syntax-highlighter/dist/esm/styles/prism/okaidia';
-
-SyntaxHighlighter.registerLanguage('jsx', jsx);
 
 const ReactCodeBlock = ({ value }) => (
   <SyntaxHighlighter language='jsx' style={okaidia}>
