b4fc61c07a4ec41d07f9952dfe345e90f9174649
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-11-21 11:17:06 +0300

    updated navigation markup by wrapping nav tag

--- components/Navigation/NavigationList/index.js
+++ components/Navigation/NavigationList/index.js
@@ -4,36 +4,38 @@ import { pages } from '../../../utils/links';
 import theme from '../../../styles/theme';
 
 const NavigationList = () => (
-  <ul>
-    { Object.entries(pages).map(([key, value]) => (
-      <NavitationListItem
-        key={key}
-        title={value.title}
-        href={value.href}
-        as={value.as} />
-    )) }
-    <style jsx>{`
-      ul {
-        display: flex;
-        font-size: 16px;
-      }
-      ul:hover {
-        color: #9e9e9e;
-        ${theme.linkHoveringEffect}
-      }
-      @media screen and (max-width: ${theme.breakpoint.tablet - 1}px) {
+  <nav>
+    <ul>
+      { Object.entries(pages).map(([key, value]) => (
+        <NavitationListItem
+          key={key}
+          title={value.title}
+          href={value.href}
+          as={value.as} />
+      )) }
+      <style jsx>{`
         ul {
-          position: absolute;
-          left: 0;
-          right: 0;
-          top: 64px;
-
-          width: 100%;
-          justify-content: space-evenly;
+          display: flex;
+          font-size: 16px;
         }
-      }
-    `}</style>
-  </ul>
+        ul:hover {
+          color: #9e9e9e;
+          ${theme.linkHoveringEffect}
+        }
+        @media screen and (max-width: ${theme.breakpoint.tablet - 1}px) {
+          ul {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 64px;
+
+            width: 100%;
+            justify-content: space-evenly;
+          }
+        }
+      `}</style>
+    </ul>
+  </nav>
 );
 
 export default NavigationList;
